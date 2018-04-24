ce23d581e2049beedfdf68c966f7e3bf8ee8cc4d
bryanwuAC <wuyuncheng1995112@gmail.com>
2018-04-24 16:46:12 -0500

    Add token_zh and modify split_data.

--- transformer/split_data.py
+++ transformer/split_data.py
@@ -10,21 +10,21 @@ def split_data(file_path):
     with open(file_path, encoding='utf8') as file:
         line_num = 0
         for line in file:
-            if line != '\n':
-                if line_num <TEST_SET_SIZE:
-                    test_set.append(line)
-                elif line_num < TRAIN_SET_SIZE + TEST_SET_SIZE:
-                    train_set.append(line)
-                else:
-                    break
-                line_num += 1
-    with open(file_path + "_test", mode = 'w', encoding='utf8') as file:
+            if line_num < TEST_SET_SIZE:
+                test_set.append(line)
+            elif line_num < TRAIN_SET_SIZE + TEST_SET_SIZE:
+                train_set.append(line)
+            else:
+                break
+            line_num += 1
+    with open(file_path + "_test", mode='w', encoding='utf8') as file:
         for line in test_set:
             file.write("%s" % line)
 
-    with open(file_path + "_train", mode = 'w', encoding='utf8') as file:
+    with open(file_path + "_train", mode='w', encoding='utf8') as file:
         for line in train_set:
             file.write("%s" % line)
+
 
 if __name__ == '__main__':
     split_data(hp.source)
