94eb30edc2b5a6bdf61050cd4a6eaddc2a95a526
bzhang70 <bzhang70@illinois.edu>
2018-04-30 18:56:07 -0500

    mod

--- transformer/train.py
+++ transformer/train.py
@@ -171,7 +171,7 @@ class Graph():
                 for i in range(hp.num_blocks):
                     with tf.variable_scope("num_blocks_{}".format(i)):
                         ## Multihead Attention ( self-attention)
-                        self.dec2_1 = multihead_attention(queries=self.dec2,
+                        self.dec2 = multihead_attention(queries=self.dec2,
                                                         keys=self.dec2,
                                                         num_units=hp.hidden_units,
                                                         num_heads=hp.num_heads,
@@ -181,8 +181,8 @@ class Graph():
                                                         scope="self_attention")
 
                         ## Multihead Attention ( encoder attention)
-                        self.dec2_2 = multihead_attention(queries=self.dec2,
-                                                        keys=enc_list[i],
+                        self.dec2 = multihead_attention(queries=self.dec2,
+                                                        keys=self.enc,
                                                         num_units=hp.hidden_units,
                                                         num_heads=hp.num_heads,
                                                         dropout_rate=hp.dropout_rate,
@@ -190,20 +190,20 @@ class Graph():
                                                         causality=False,
                                                         scope="encoder_attention")
                         ## Multihead Attention ( decoder attention)
-                        self.dec2_3 = multihead_attention(queries=self.dec2,
-                                                        keys=dec_list[i],
-                                                        num_units=hp.hidden_units,
-                                                        num_heads=hp.num_heads,
-                                                        dropout_rate=hp.dropout_rate,
-                                                        is_training=is_training,
-                                                        causality=False,
-                                                        scope="decoder_attention")
-                        self.dec2 = tf.divide(tf.add_n([self.dec2_1, self.dec2_2, self.dec2_3]), 3.0)
+                        # self.dec2 = multihead_attention(queries=self.dec2,
+                        #                                 keys=self.dec1,
+                        #                                 num_units=hp.hidden_units,
+                        #                                 num_heads=hp.num_heads,
+                        #                                 dropout_rate=hp.dropout_rate,
+                        #                                 is_training=is_training,
+                        #                                 causality=False,
+                        #                                 scope="decoder_attention")
+                        # self.dec2 = tf.add_n([self.dec2_1, self.dec2_2, self.dec2_3])
                         ## Feed Forward
                         self.dec2 = feedforward(self.dec2, num_units=[4*hp.hidden_units, hp.hidden_units])
 
             # Final linear projection
-            self.logits = tf.layers.dense(self.dec1, len(en2idx))
+            self.logits = tf.layers.dense(self.dec2, len(en2idx))
             self.preds = tf.to_int32(tf.arg_max(self.logits, dimension=-1))
             self.istarget = tf.to_float(tf.not_equal(self.y, 0))
             self.acc = tf.reduce_sum(tf.to_float(tf.equal(self.preds, self.y))*self.istarget)/ (tf.reduce_sum(self.istarget))
